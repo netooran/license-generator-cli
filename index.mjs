1271c4e6ee49b9a90083a41486034c87e284ddaf
Sarath V S <sarathvss65@gmail.com>
2019-12-27 15:39:35 +0530

    First commit

---
+++ index.mjs
@@ -0,0 +1,24 @@
+"use strict";
+
+import * as fs from "fs";
+import * as path from "path";
+import prependFile from "prepend-file";
+
+function walkFiles(dir, callback) {
+  fs.readdir(dir, (err, files) => {
+    if (err) throw new Error(err);
+    files.forEach(file => {
+      let filePath = path.join(dir, file);
+      const stat = fs.lstatSync(filePath);
+      if (stat.isFile()) callback(filePath);
+      if (stat.isDirectory()) walkFiles(filePath, callback);
+    });
+  });
+}
+
+export default function makeLicense(notice, dir = "./") {
+  return new Promise((resolve, reject) => {
+    walkFiles(dir, file => prependFile(file, `${notice}\n`));
+    resolve("Success");
+  });
+}
